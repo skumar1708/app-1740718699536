b42db3fa953b8a219df28394be0868dcdaa53806
Sharvan Sharma <itbhu.sk@gmail.com>
2025-02-28 10:29:45 +0530

    Automated commit: frontend/src/components/Header.js

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Bhagavad Gita - Chapter 1</h1>
+    </header>
+  );
+}
+
+export default Header;
